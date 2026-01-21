951e59d2e314e1b9b179d0ae2fe3fefeb08511e2
tdean68 <timdean68@gmail.com>
2026-01-20 19:11:37 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,17 @@
+
+//Timothy Dean
+// Assignment 1
+
+public class First {
+    public static void main(String [] args) {
+        String name = "Tim";
+        String JobTitle = "Computer and Information Systems Manager or IT Manager";
+        String why = "Currently working in the IT industry. I eventually want to grow and become an IT Manager of some sort";
+        String MedianIncome = "$171,200";
+
+        System.out.println("Hello " + name + "!");
+        System.out.println("Job Title: " + JobTitle);
+        System.out.println("Why: " + why);
+        System.out.println("2024 Median Income: " + MedianIncome);
+    }
+}
