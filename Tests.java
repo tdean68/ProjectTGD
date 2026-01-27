a652f1f7feb52fdbd8c54e497b5ca0708a35c833
tdean68 <timdean68@gmail.com>
2026-01-27 15:51:13 -0500

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,23 @@
+// Module 2 Assignment 1
+// Timothy Dean
+
+public class Tests
+
+{
+	public static void main(String [] args)
+	{
+
+	double test1 = 95.0;
+    double test2 = 79.5;
+	double test3 = 85.5;
+		
+	double average = (test1 + test2 + test3) / 3;
+
+    System.out.println("Test score 1: " + test1);
+    System.out.println("Test score 2: " + test2);
+    System.out.println("Test score 3: " + test3);
+	System.out.printf("The average of 3 test scores is: %.2f%n", average);
+
+	}
+    
+}
