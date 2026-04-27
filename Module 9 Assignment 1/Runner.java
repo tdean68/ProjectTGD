5c98c67ac8de678946d2e822b93f22e4bd2468dc
tdean68 <timdean68@gmail.com>
2026-04-27 15:11:07 -0400

    Add files via upload

---
+++ Module 9 Assignment 1/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    ///Instantiate a Towers object to swap the rings on 3 "towers"
+    public static void main(String[] args)
+    {
+        Towers towersOfHanoi = new Towers(3);
+    }
+}
